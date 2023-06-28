58cc0580eda595f816b5c5b7595fa44ad8ce7e00
welkin22 <136572398+welkin22@users.noreply.github.com>
2023-06-28 16:43:23 +0800

    feature: use docker compose to deploy node (#2)
    * feature: use docker compose to deploy node
    
    * remove static url
    
    * add genesis file into repo
    
    * add jwt.txt into repo
    ensure line break end of file
    
    * remove $@
    
    * remove opbnb ignore
    
    * R uppercase
    
    * fix /scripts/op-geth-start.sh
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Welkin <welkin.b@nodereal.com>
    Co-authored-by: Owen <owen.h@nodereal.io>

---
+++ scripts/op-geth-start.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+set -eou
+
+if [ ! -d $BEDROCK_DATADIR/geth ]; then
+  exec geth --datadir $BEDROCK_DATADIR init genesis.json
+else
+   echo "$BEDROCK_DATADIR/geth exists."
+fi
+
+# Start op-geth.
+exec geth \
+  --datadir="$BEDROCK_DATADIR" \
+  --verbosity=3 \
+  --http \
+  --http.corsdomain="*" \
+  --http.vhosts="*" \
+  --http.addr=0.0.0.0 \
+  --http.port=8545 \
+  --http.api=net,eth,engine \
+  --ws \
+  --ws.addr=0.0.0.0 \
+  --ws.port=8545 \
+  --ws.origins="*" \
+  --ws.api=eth,engine \
+  --syncmode=full \
+  --maxpeers=10 \
+  --networkid=$CHAIN_ID \
+  --miner.gaslimit=150000000 \
+  --triesInMemory=32 \
+  --txpool.globalslots=10000 \
+  --txpool.globalqueue=5000 \
+  --txpool.accountqueue=200 \
+  --txpool.accountslots=200 \
+  --cache 32000 \
+  --cache.preimages \
+  --allow-insecure-unlock \
+  --authrpc.addr="0.0.0.0" \
+  --authrpc.port="8551" \
+  --authrpc.vhosts="*" \
+  --authrpc.jwtsecret=./jwt.txt \
+  --gcmode=archive \
+  --metrics \
+  --metrics.port 6060 \
+  --metrics.addr 0.0.0.0 \
+  --rollup.sequencerhttp=$L2_RPC
