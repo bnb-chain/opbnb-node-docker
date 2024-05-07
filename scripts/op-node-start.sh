1a5784d7f08486c0bd19b2e855ac892456fbe1f7
redhdx <lredhdx@gmail.com>
2024-05-07 17:20:30 +0800

    feat: update opbnb node docker script

--- scripts/op-node-start.sh
+++ scripts/op-node-start.sh
@@ -1,43 +1,18 @@
 #!/bin/sh
 set -eou
 
-apk add wget
 if [ "$NETWORK_NAME" == "testnet" ]; then
   export NETWORK=opBNBTestnet
-  export P2P_BOOTNODES="enr:-J24QGQBeMsXOaCCaLWtNFSfb2Gv50DjGOKToH2HUTAIn9yXImowlRoMDNuPNhSBZNQGCCE8eAl5O3dsONuuQp5Qix2GAYjB7KHSgmlkgnY0gmlwhDREiqaHb3BzdGFja4PrKwCJc2VjcDI1NmsxoQL4I9wpEVDcUb8bLWu6V8iPoN5w8E8q-GrS5WUCygYUQ4N0Y3CCIyuDdWRwgiMr,enr:-J24QJKXHEkIhy0tmIk2EscMZ2aRrivNsZf_YhgIU51g4ZKHWY0BxW6VedRJ1jxmneW9v7JjldPOPpLkaNSo6cXGFxqGAYpK96oCgmlkgnY0gmlwhANzx96Hb3BzdGFja4PrKwCJc2VjcDI1NmsxoQMOCzUFffz04eyDrmkbaSCrMEvLvn5O4RZaZ5k1GV4wa4N0Y3CCIyuDdWRwgiMr"
 fi
 
 if [ "$NETWORK_NAME" == "mainnet" ]; then
   export NETWORK=opBNBMainnet
-  export P2P_BOOTNODES="enr:-J24QPSZMaGw3NhO6Ll25cawknKcOFLPjUnpy72HCkwqaHBKaaR9ylr-ejx20INZ69BLLj334aEqjNHKJeWhiAdVcn-GAYv28FmZgmlkgnY0gmlwhDTDWQOHb3BzdGFja4PMAQCJc2VjcDI1NmsxoQJ-_5GZKjs7jaB4TILdgC8EwnwyL3Qip89wmjnyjvDDwoN0Y3CCIyuDdWRwgiMs,enr:-J24QA9sgVxbZ0KoJ7-1gx_szfc7Oexzz7xL2iHS7VMHGj2QQaLc_IQZmFthywENgJWXbApj7tw7BiouKDOZD4noWEWGAYppffmvgmlkgnY0gmlwhDbjSM6Hb3BzdGFja4PMAQCJc2VjcDI1NmsxoQKetGQX7sXd4u8hZr6uayTZgHRDvGm36YaryqZkgnidS4N0Y3CCIyuDdWRwgiMs"
 fi
 
 # Start op-node.
 exec op-node \
-  --l1.trustrpc \
-  --sequencer.l1-confs=15 \
-  --verifier.l1-confs=15 \
-  --l1.http-poll-interval 3s \
-  --l1.epoch-poll-interval 45s \
-  --l1.rpc-max-batch-size 20 \
   --network=$NETWORK \
-  --rpc.addr=0.0.0.0 \
-  --rpc.port=8546 \
-  --p2p.sync.req-resp \
-  --p2p.listen.ip=0.0.0.0 \
-  --p2p.listen.tcp=9003 \
-  --p2p.listen.udp=9003 \
   --snapshotlog.file=./snapshot.log \
-  --p2p.priv.raw=$P2P_PRIV_KEY \
-  --p2p.bootnodes=$P2P_BOOTNODES \
-  --metrics.enabled \
-  --metrics.addr=0.0.0.0 \
-  --metrics.port=7300 \
-  --pprof.enabled \
-  --rpc.enable-admin \
-  --l1=$OP_NODE__RPC_ENDPOINT \
-  --l2=http://op-geth:8551 \
   --l2.jwt-secret=./jwt.txt \
-  --l2.engine-sync=true \
-  --l2.skip-sync-start-check=true \
-  --log.level=debug
+  --l1=$OP_NODE__RPC_ENDPOINT \
+  --l2=http://op-geth:8551
